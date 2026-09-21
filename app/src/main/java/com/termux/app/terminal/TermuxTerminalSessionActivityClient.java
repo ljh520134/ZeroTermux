4c91cc3459e80df350772f274384c210d409c046
ljh520134 <ljh520134@users.noreply.github.com>
2026-09-21 17:46:01 +0800

    [skip ci] 修复后台会话标题更新反复弹出 Toast

--- app/src/main/java/com/termux/app/terminal/TermuxTerminalSessionActivityClient.java
+++ app/src/main/java/com/termux/app/terminal/TermuxTerminalSessionActivityClient.java
@@ -139,13 +139,6 @@ public class TermuxTerminalSessionActivityClient extends TermuxTerminalSessionCl
     public void onTitleChanged(@NonNull TerminalSession updatedSession) {
         if (!mActivity.isVisible()) return;
 
-        if (updatedSession != mActivity.getCurrentSession()) {
-            // Only show toast for other sessions than the current one, since the user
-            // probably consciously caused the title change to change in the current session
-            // and don't want an annoying toast for that.
-            mActivity.showToast(toToastTitle(updatedSession), true);
-        }
-
         termuxSessionListNotifyUpdated();
     }
 
